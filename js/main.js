a137ffae0304f9a0f1f0c82a537951ce2dac2083
AlexABA91 <80538104+AlexABA91@users.noreply.github.com>
2021-06-08 13:07:08 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
-$(function() {
+$(function () {
 	$('.slikslider').slick({
-		dots:true,
-		speed:500,
-		easing:'ease',
+		dots: true,
+		speed: 500,
+		easing: 'ease',
 		draggable: false,
-		infinite:true,
+		infinite: true,
 		slidesToShow: 1,
 	});
-	$('.arrowap').click(function(){
-		$('html, body').animate({scrollTop:0}, '500');
-    });	
+	$('.arrowap').click(function () {
+		$('html, body').animate({ scrollTop: 0 }, '500');
+	});
 });
+
+let search = document.querySelector('.fa-search');
+let string = document.querySelector('.search-1')
+search.onclick = () => {
+	string.classList.toggle('active')
+}
